900799c1ad11a376b69f2a5441fbe185b67214ee
Tharki-God <alokender018@gmail.com>
2024-04-12 03:53:31 +0530

    better

--- src/patches/Slate.tsx
+++ src/patches/Slate.tsx
@@ -15,17 +15,20 @@ export default (): void => {
           c?.props?.className?.includes("channelTextArea") && Array.isArray(c?.props?.children),
       ) as Types.ReactTree;
       const [textValue, setTextValue] = React.useState(text);
+      const [shouldShow, setShouldShow] = React.useState(false);
       React.useEffect(() => {
         setTextValue(text);
       }, [text]);
-
-      if (
-        (!channel.isDM() &&
-          !channel.isGroupDM() &&
-          !PermissionStore.canBasicChannel(DiscordConstants.Permissions.SEND_MESSAGES, channel)) ||
-        !container ||
-        container?.props?.children.some((c) => c?.key === "wpm")
-      )
+      React.useEffect(() => {
+        setShouldShow(
+          Boolean(
+            channel.isDM() ||
+              channel.isGroupDM() ||
+              PermissionStore.canBasicChannel(DiscordConstants.Permissions.SEND_MESSAGES, channel),
+          ),
+        );
+      }, [channel.id]);
+      if (!shouldShow || !container || container?.props?.children.some((c) => c?.key === "wpm"))
         return res;
       container?.props?.children.push(<WPMCounter textValue={textValue} key="wpm" />);
       return res;
