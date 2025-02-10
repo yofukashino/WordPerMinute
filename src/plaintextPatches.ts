6ea64ead71cc3862baa824998ddb577e37755695
yofukashino <alokender018@gmail.com>
2025-02-11 01:16:59 +0530

    fix: plaintextPatches

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -2,16 +2,16 @@ import Types from "./types";
 
 export default [
   {
-    find: "this.getCooldownTextStyle()",
+    find: '"TypingUsers"',
     replacements: [
       {
         match: /(channelId:.\.id}\))]/,
         replace: (_: string, prefix: string) =>
-          `${prefix},replugged.plugins.getExports("dev.yofukashino.WordPerMinute")?._WPM?.(this.props)]`,
+          `${prefix},replugged.plugins.getExports("dev.yofukashino.WordPerMinute")?._WPM?.(arguments[0])]`,
       },
       {
         match: /\((0===.\.length)/,
-        replace: (_: string, suffix: string) => `(!this?.props?.channel?.id&&${suffix}`,
+        replace: (_: string, suffix: string) => `(!arguments?.[0]?.channel?.id&&${suffix}`,
       },
     ],
   },
