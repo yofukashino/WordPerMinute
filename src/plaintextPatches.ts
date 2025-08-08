b3a323d0ff2f36b975960e540d20ee464d45a2bd
yofukashino <alokender018@gmail.com>
2025-08-08 06:04:49 +0530

    fix: plaintextPatch

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -10,8 +10,8 @@ export default [
           `${prefix},replugged.plugins.getExports("dev.yofukashino.WordPerMinute")?._WPM?.(arguments[0])]`,
       },
       {
-        match: /\((0===.\.length)/,
-        replace: (_: string, suffix: string) => `(!arguments?.[0]?.channel?.id&&${suffix}`,
+        match: /\),(0===.\.length)/,
+        replace: (_: string, suffix: string) => `),!arguments?.[0]?.channel?.id&&${suffix}`,
       },
     ],
   },
